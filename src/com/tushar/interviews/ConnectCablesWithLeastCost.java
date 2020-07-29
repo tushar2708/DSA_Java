e3d1aaf7bb908fd9e585dec803b464846493f1c1
Tushar Dwivedi <tdwivedi2708@gmail.com>
2020-07-29 06:54:26 +0530

    added wire connection greedy algo question

---
+++ src/com/tushar/interviews/ConnectCablesWithLeastCost.java
@@ -0,0 +1,59 @@
+package com.tushar.interviews;
+
+/*
+Question:
+1. You are given a set of wires, with different lengths.
+2. Cost of connecting any 2 wires is equal to sum of their lengths
+3. Any 2 wires can be connected, either from the original ones, or the ones formed by connecting other wires
+Eg. [1,4,2,7,8]
+1+5 = 5
+now it becomes:
+[5,2,7,8]
+ */
+
+/*
+Solution:
+
+1. It an be done with a greedy approach.
+2. The key is that we have to connect 2 smallest possible wires at any stage, to keep the overall cost low.
+3. So I will use a min heap, and will always take 2 smallest wires from the heap,
+ and will push the new wire to the heap as well.
+ */
+
+import java.util.PriorityQueue;
+
+public class ConnectCablesWithLeastCost {
+    public static int findLowestPossibleCost(int[] wireLengths) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+//        Collections.addAll(minHeap, Arrays.asList(wireLengths));
+
+        for (int wireLen :
+                wireLengths) {
+            minHeap.add(wireLen);
+        }
+
+        int cost = 0;
+
+        while (!minHeap.isEmpty()) {
+            int a = minHeap.remove();
+            int c;
+            if (!minHeap.isEmpty()) {
+                int b = minHeap.remove();
+                c = a + b;
+                minHeap.add(c);
+            } else {
+                c = a;
+            }
+            cost += c;
+        }
+
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        int[] wires = {13, 5, 6, 7, 1, 9, 2};
+
+        System.out.println("cost: " + findLowestPossibleCost(wires));
+    }
+}
